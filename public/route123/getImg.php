04acd90fc593f9d602c2c56ba462b352547aa9bf
HawkinsPD <aintnomistakesallowed@gmail.com>
2023-06-27 00:51:02 +0300

    ave

--- public/route123/getImg.php
+++ public/route123/getImg.php
@@ -25,18 +25,11 @@ class GetImg implements RouteInterface
 {
     public function route(): string
     {
-    $imagesArray = [
-        'red' => 'Taylor_Swift_-_Red.png',
-        'evermore' => 'Taylor_Swift_-_Evermore.png',
-        'midnights' => 'Taylor_Swift_—_Midnights.png'
-    ];
     return <<<HTML
     <script type="text/javascript" src="functions.js"></script>
  
     <script>
         getImg();
-        //inside /getImages   imagesArray foreach[imagesArray create html anchor]
-        //addAnchorArray($imagesArray);
     </script>
 
 HTML;
